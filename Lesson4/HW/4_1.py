1a6ec15eff56b7f2464f8c860d27397ff229ca67
Sergey <demonvatake@gmail.com>
2022-10-25 11:11:27 +0300

    4_1

---
+++ Lesson4/HW/4_1.py
@@ -0,0 +1,11 @@
+# 1. Вычислить число c заданной точностью d
+
+from decimal import Decimal
+
+
+def accuracy(num, acc):
+    number = Decimal(f"{num}")
+    return number.quantize(Decimal(f"{acc}"))
+
+
+print(accuracy(float(input("Enter a real number: ")), float(input("Enter the required accuracy 0.0001: "))))
